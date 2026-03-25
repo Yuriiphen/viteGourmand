a59f73141dfcf06ac7c918fa0c79bee7264d9db7
Yuriiphen <yuriijohnny@gmail.com>
2026-03-25 12:43:56 +0100

    Changement MySQL -> Mariadb pour la connexion à la base de données.

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $port = "3307";
 $dbname = "vite_gourmand";
 $username = "root";
-$password = "";
+$password = "0000";
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
